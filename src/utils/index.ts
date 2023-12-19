3e72e25e24c77ca817bc9293be28a0f4062d12f9
Vic <syuan0212@gmail.com>
2023-12-19 22:32:38 +0800

    feat: add generateEmailToken

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,3 +6,23 @@ export const generateToken = (payload: { userId?: string }) => {
     expiresIn: process.env.JWT_EXPIRES_DAY,
   });
 };
+
+export const generateEmailToken = () => {
+  const code = generateRandomCode();
+
+  const token = jsonWebToken.sign({ code }, process.env.JWT_SECRET || "", {
+    expiresIn: 3600,
+  });
+
+  return { code, token };
+};
+
+const generateRandomCode = () => {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let code = "";
+  for (let i = 0; i < 6; i++) {
+    code += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return code;
+};
